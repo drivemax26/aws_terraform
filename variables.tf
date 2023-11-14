341360577a5d99158a3aefff78e60dfccd2bed2e
Oleh Fadieiev <61756426+drivemax26@users.noreply.github.com>
2023-11-14 17:04:38 +0200

    Solution

--- variables.tf
+++ variables.tf
@@ -14,8 +14,8 @@ variable "cidr" {
 }
 
 variable "publicCIDR" {
-  type    = list(string)
-  default = ["10.0.1.0/24"]
+  # type    = list(string)
+  default = "10.0.1.0/24"
 }
 
 variable "environment" {
